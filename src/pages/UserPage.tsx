f05527c89f63f67b39fbb7aee87ff342230bd2cf
Karen MacPherson <karen@enveritas.org>
2024-09-06 10:10:08 -0400

    Initialize Repo with UserPage

---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,11 @@
+import UserInfo from "../components/UserInfo/UserInfo";
+
+const UserPage = () => {  
+  return <>
+    <h1>User Page</h1>
+    <p>Update your user information</p>
+    <UserInfo />
+  </>
+}
+
+export default UserPage;
